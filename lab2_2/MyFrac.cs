5e8ed7590654ed5ae521ba4cd4c4623e57d59a49
Rostik <91599400+RostislavTriare@users.noreply.github.com>
2021-10-26 22:52:29 +0300

    Add files via upload

---
+++ lab2_2/MyFrac.cs
@@ -0,0 +1,147 @@
+﻿using System;
+namespace Laba2_2
+{
+    class MyFrac
+    {
+
+        public int nom=0, denom=0;
+
+        
+        public MyFrac(int nom, int denom)
+        {
+
+            this.nom = Math.Abs(nom);
+            this.denom = Math.Abs(denom);
+            
+        }
+        public String Cheloe()
+        {
+            int k = 0;
+            int nom1 = nom;
+            int denom1 = denom;
+            while ((nom1/denom1)>=1)
+            {
+                nom1 = nom1 - denom1;
+                k++;
+            }
+            
+
+            return $"Ціла части = {k}";
+        }
+        public double MyDrob()
+        {
+            
+            double dill = (double)nom/ denom;
+     
+            return dill;
+        }
+
+        public double Sum()
+        {
+            int nom1 = nom;
+            int denom1 = denom;
+
+            int nom2 = 20;
+            int denom2 = 17;
+
+            double rez = (double)(nom1 * denom2 + nom2*denom1)/(denom1*denom2);
+            return rez;
+           
+        }
+
+        public double Minum()
+        {
+            int nom1 = nom;
+            int denom1 = denom;
+
+            int nom2 = 20;
+            int denom2 = 17;
+
+            double rez= (double)(nom1 * denom2 - nom2 * denom1) / (denom1 * denom2);
+            return rez;
+
+        }
+
+        public double Mnog()
+        {
+            int nom1 = nom;
+            int denom1 = denom;
+
+            int nom2 = 20;
+            int denom2 = 17;
+
+            double rez = (double)(nom1 *  nom2 ) / (denom1 * denom2);
+            return rez;
+
+        }
+        public double Devide()
+        {
+            int nom1 = nom;
+            int denom1 = denom;
+
+            int nom2 = 20;
+            int denom2 = 17;
+
+            double rez = (double)(nom1 * denom2) / (denom1 * nom2);
+            return rez;
+
+        }
+        public int GetRGR113LeftSum()
+        {
+            
+            double rez = 0.00;
+            
+            int i = 0;
+            while (i <= 9){ rez +=(1.00 / (2.00 * i + 1.00)); i++;  }
+            
+            return (int)rez;
+
+        }
+        public double GetRGR115LeftSum()
+        {
+
+            double rez = 1.00;
+
+            int i = 0;
+            while (i <= 9) { rez *= (1.00 / (2.00 * i + 1.00)); i++; }
+
+            return rez;
+
+        }
+        public int normal()
+        {
+            int x, y, NOD;
+            x = nom;
+            y = denom;
+            while (Math.Abs(x) != Math.Abs(y))
+            {
+                if (x > y)
+                    x = x - y;
+                else y = y - x;
+            }
+            NOD = x;
+            return x;
+        }
+        
+        
+        public override String ToString()
+        {
+            String num;
+            int a = nom, b = denom;
+            int dil = normal();
+            denom = denom / dil;
+            nom = nom / dil;
+            if (dil == 1)
+            {
+                num = ("0");
+            }
+            else
+            {
+                num = ($"Первинний = {a}/{b} Результат = {nom}/{denom}");
+            }
+            return num;
+        }
+        
+
+    }
+}
---
+++ lab2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Laba2_2
+{
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyFrac my= new MyFrac(28,27);
+            Console.WriteLine(my.nom);
+            Console.WriteLine(my.denom);
+            Console.WriteLine(my.Cheloe());
+            Console.WriteLine(my.MyDrob());
+            Console.WriteLine(my.Sum());
+            Console.WriteLine(my.Minum());
+            Console.WriteLine(my.Mnog());
+            Console.WriteLine(my.Devide());
+            Console.WriteLine(my.GetRGR113LeftSum());
+            Console.WriteLine(my.GetRGR115LeftSum());
+            Console.WriteLine(my.ToString());
+           
+
+        }
+    }
+}
